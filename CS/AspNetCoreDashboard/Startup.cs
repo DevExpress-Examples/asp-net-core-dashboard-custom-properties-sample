2ee25326a359f14e51cd024ec4c34cb5695d6a9a
Sergey Kanaev <31354372+sergey-kanaev@users.noreply.github.com>
2021-01-27 11:49:01 +0300

    Update Startup.cs

--- CS/AspNetCoreDashboard/Startup.cs
+++ CS/AspNetCoreDashboard/Startup.cs
@@ -73,9 +73,8 @@ namespace CustomPropertiesSample {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            app.UseDeveloperExceptionPage();
             if(env.IsDevelopment()) {
-                
+                app.UseDeveloperExceptionPage();
             }
             else {
                 app.UseExceptionHandler("/Home/Error");
